a29bdb3272873187096d69cba02f99742d583345
Renato Pessoa <renatopessoaneto@gmail.com>
2019-10-09 23:05:57 -0300

    part2e erro message component and style

--- part2/course-class/src/App.js
+++ part2/course-class/src/App.js
@@ -2,12 +2,14 @@ import React, {useState, useEffect} from 'react'
 
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/Notification'
 
 
 const App = () => {
     const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('')
     const [showAll, setShowAll] = useState(true)
+    const [errorMessage, setErrorMessage] = useState('some error happened...')
 
     useEffect(() => {
         console.log('effect')
@@ -60,9 +62,13 @@ const App = () => {
                 setNotes(notes.map(note => note.id !== id ? note : returnedNote))
             })
             .catch(error => {
-                alert(
-                    `the note '${note.content}' was already deleted from the server`
+                setErrorMessage(
+                    `Note '${note.content}' was already removed from the server`
                 )
+                setTimeout(() => {
+                   setErrorMessage(null) 
+                }, 5000)
+                setNotes(notes.filter(n => n.id !== id))
             })
     }
 
@@ -76,6 +82,9 @@ const App = () => {
     return (
         <div>
             <h1>Notes</h1>
+
+            <Notification message={errorMessage} />
+
             <div>
                 <button onClick={() => setShowAll(!showAll)}>
                     show {showAll ? 'important' : 'all'}
