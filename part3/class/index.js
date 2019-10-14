02815e7cccfc05539cd79ecab03ec6a43770972a
Renato Pessoa <renatopessoaneto@gmail.com>
2019-10-14 16:37:36 -0300

    added the middlewares from class

--- part3/class/index.js
+++ part3/class/index.js
@@ -25,63 +25,78 @@ let notes = [
   ]
 
 
-  app.use(bodyParser.json())
+app.use(bodyParser.json())
 
-  app.get('/', (request, response) => {
-      response.send('<h1>Hello World!</h1>')
-  })
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path: ', request.path)
+  console.log('Body: ', request.body)
+  console.log('---')
+  next()
+}
 
-  app.get('/notes', (request,response) =>{
-      response.json(notes)
-  })
+app.use(requestLogger)
 
-  app.get('/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
 
-    if(note) {
-      response.json(note)
-    }else{
-      response.status(404).end()
-    }
-  })
+app.get('/notes', (request,response) =>{
+    response.json(notes)
+})
 
-  app.delete('/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+app.get('/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
 
-    response.status(204).end()
-  })
+  if(note) {
+    response.json(note)
+  }else{
+    response.status(404).end()
+  }
+})
 
-  const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-      return maxId + 1
+app.delete('/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+    return maxId + 1
+}
+
+app.post('/notes', (request, response) => {
+
+  const body = request.body
+
+  if(!body.content){
+    return response.status(400).json({
+      error:'content missing'
+    })
   }
 
-  app.post('/notes', (request, response) => {
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
 
-    const body = request.body
+  notes = notes.concat(note)
 
-    if(!body.content){
-      return response.status(400).json({
-        error:'content missing'
-      })
-    }
+  response.json(note)
+})
 
-    const note = {
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-      id: generateId(),
-    }
 
-    notes = notes.concat(note)
-
-    response.json(note)
-  })
-
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 
 const port = 3001
 app.listen(port)
