6eca3f683f7ccf60b1f55c54a131e7d9051b1a31
Renato Pessoa <renatopessoaneto@gmail.com>
2019-10-15 15:48:21 -0300

    upto streamlining deploying of the frontend

---
+++ part3/class/build/precache-manifest.b6a0652ae278bd096135a0afae303e89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1aecde9c42f36f06a1347e6b545b5d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "576788dda9433ceff48a",
+    "url": "/static/css/main.7c0213dd.chunk.css"
+  },
+  {
+    "revision": "1598ea12a83a741716b3",
+    "url": "/static/js/2.c26c1d31.chunk.js"
+  },
+  {
+    "revision": "576788dda9433ceff48a",
+    "url": "/static/js/main.378c9eca.chunk.js"
+  },
+  {
+    "revision": "1f9c8b55e7a48244d889",
+    "url": "/static/js/runtime-main.6f63ef54.js"
+  }
+]);
