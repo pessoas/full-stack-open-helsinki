02a9c7b60156a9bd52e036a9c9d622680300360c
Renato Pessoa <renatopessoaneto@gmail.com>
2019-10-17 16:55:21 -0300

    updated get all notes and get single note

--- part3/class/index.js
+++ part3/class/index.js
@@ -8,51 +8,6 @@ const Note = require('./models/note')
 
 const app = express()
 
-//DATABASE CONFIG
-/*
-
-const url = `mongodb+srv://fullstack:${password}@cluster0-2e1jk.mongodb.net/note-app?retryWrites=true&w=majority`
-
-mongoose.connect(url, { useNewUrlParser: true })
-
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)*/
-
-/*
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]*/
-
 app.use(express.static('build'))
 app.use(bodyParser.json())
 
@@ -78,14 +33,9 @@ app.get('/api/notes', (request,response) =>{
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if(note) {
-    response.json(note)
-  }else{
-    response.status(404).end()
-  }
+  Note.findById(request.params.id).then(note => {
+    response.json(note.toJSON)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -106,22 +56,21 @@ app.post('/api/notes', (request, response) => {
 
   const body = request.body
 
-  if(!body.content){
+  if(body.content === undefined){
     return response.status(400).json({
       error:'content missing'
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote.toJSON())
+  })
 })
 
 
