4cc3098b0b192ce94524316eea2d8a7a2ae46477
Renato Pessoa <renatopessoaneto@gmail.com>
2019-10-21 15:43:01 -0300

    created notes validation on post

--- part3/class/index.js
+++ part3/class/index.js
@@ -53,15 +53,9 @@ app.delete('/api/notes/:id', (request, response, next) => {
   
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
 
   const body = request.body
-
-  if(body.content === undefined){
-    return response.status(400).json({
-      error:'content missing'
-    })
-  }
 
   const note = new Note({
     content: body.content,
@@ -69,9 +63,11 @@ app.post('/api/notes', (request, response) => {
     date: new Date(),
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote.toJSON())
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote.toJSON())
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -102,6 +98,8 @@ const errorHandler = (error, request, response, next) => {
 
   if(error.name === 'CastError' && error.kind === 'ObjectId'){
     return response.status(400).send({ error: 'malformatted id' })
+  }else if(error.name === 'ValidationError'){
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 }
