a47a700b181af3825928f62e9e1ccb16547e4483
Renato Pessoa <renatopessoaneto@gmail.com>
2019-10-22 12:01:45 -0300

    copied note app project files to part4

---
+++ part4/notes-app-class/models/note.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+mongoose.set('useAndModify', false)
+
+const { url } = require('../config')
+
+console.log('connecting to', url)
+
+mongoose.connect(url, { useNewUrlParser: true })
+  .then(result => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB: ', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minlength: 5,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
