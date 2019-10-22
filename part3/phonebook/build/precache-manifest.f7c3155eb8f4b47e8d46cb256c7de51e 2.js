8f10330e75f1a3de187faec8445508af670f561c
Renato Pessoa <renatopessoaneto@gmail.com>
2019-10-22 13:58:23 -0300

    I think it now works, copied notes app from part 3

---
+++ part3/phonebook/build/precache-manifest.f7c3155eb8f4b47e8d46cb256c7de51e 2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6917a2e7db2f3af2bcb008c8ea10fcbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e94152edbe14df3a25a8",
+    "url": "/static/js/2.f37a49f9.chunk.js"
+  },
+  {
+    "revision": "c39dada7f74091fd773c",
+    "url": "/static/js/main.a9c27031.chunk.js"
+  },
+  {
+    "revision": "4933d93a75f9dc6a5c62",
+    "url": "/static/js/runtime-main.e1c60350.js"
+  }
+]);
