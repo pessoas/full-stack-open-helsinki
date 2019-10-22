e4787ade2ce539fc42d310e12bbf20e02cd2026d
Renato Pessoa <renatopessoaneto@gmail.com>
2019-10-22 12:33:28 -0300

    deleted part 4 folder

--- part4/notes-app-class/README.md
+++
@@ -1,2 +0,0 @@
-url for the app:
-https://blooming-anchorage-50418.herokuapp.com/
--- part4/notes-app-class/config.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require('dotenv')
-
-dotenv.config()
-module.exports = {
-  password : process.env.MONGO_PASS,
-  url: process.env.MONGODB_URI,
-  PORT: process.env.PORT,
-}
