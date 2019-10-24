d9c75ac282b8c1062f66882acc64c19e377231ff
Renato Pessoa <renatopessoaneto@gmail.com>
2019-10-23 21:11:26 -0300

    created the blogs project, the files and the folders

--- part3/class/config.js
+++ part3/class/config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 module.exports = {
-  password : process.env.MONGO_PASS,
+  password: process.env.MONGO_PASS,
   url: process.env.MONGODB_URI,
   PORT: process.env.PORT,
 }
