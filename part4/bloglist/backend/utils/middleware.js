0253835498a27ec4dde0065c6db9d21fa102fe2d
Renato Pessoa <renatopessoaneto@gmail.com>
2019-10-24 23:52:02 -0300

    middleware

---
+++ part4/bloglist/backend/utils/middleware.js
@@ -0,0 +1,26 @@
+const morgan = require('morgan')
+
+morgan.token('data', (request, response) => {
+    return JSON.stringify(request.body)
+})
+
+const requestLogger = morgan(':method :url :status :res[content-length] - :response-time ms :data')
+
+const unknownEndPoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if(error.name === 'CastError' && error.kind === 'ObjectID'){
+        return response.status(400).send({ error: 'malformatted id '})
+    }
+    next(error)
+}
+
+module.exports = {
+    requestLogger,
+    unknownEndPoint,
+    errorHandler
+}
