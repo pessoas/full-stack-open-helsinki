29c1e68e57a63d4a6084a4cc33db413f8e150790
Renato Pessoa <renatopessoaneto@gmail.com>
2019-10-26 20:02:46 -0300

    configured test environment for notes-app

--- part4/notesapp/backend/app.js
+++ part4/notesapp/backend/app.js
@@ -7,9 +7,9 @@ const notesRouter = require('./controllers/notes')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
-console.log('connecting to', config.url)
+console.log('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.url, { useNewUrlParser: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
     console.log('connected to MongoDB')
   })
--- part4/notesapp/backend/utils/config.js
+++ part4/notesapp/backend/utils/config.js
@@ -1,8 +1,15 @@
 const dotenv = require('dotenv')
 
 dotenv.config()
+
+let PORT = process.env.PORT
+let MONGODB_URI = process.env.MONGODB_URI
+
+if(process.env.NODE_ENV === 'test'){
+  MONGODB_URI = process.env.TEST_MONGODB_URI
+}
+
 module.exports = {
-  password : process.env.MONGO_PASS,
-  url: process.env.MONGODB_URI,
-  PORT: process.env.PORT,
+  PORT,
+  MONGODB_URI
 }
